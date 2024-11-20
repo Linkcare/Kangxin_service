0fdec80542fae407bc1214c74bcb4f52a49352e9
Pedro Sánchez Luling <slpedro@hotmail.com>
2024-11-20 13:27:24 +0100

    LC2-10231 Reviewed for compatibility with API Version 2.7.32

--- lib/WSAPI/LinkcareSoapAPI.php
+++ lib/WSAPI/LinkcareSoapAPI.php
@@ -97,7 +97,7 @@ class LinkcareSoapAPI {
         }
 
         $date = currentDate($timezone);
-        $result = $client->session_init($user, $password, null, null, $language, '2.7.26', $reuseExistingSession ? 1 : 0, $date);
+        $result = $client->session_init($user, $password, null, null, $language, '2.7.32', $reuseExistingSession ? 1 : 0, $date);
         if ($result["ErrorCode"]) {
             throw new APIException($result["ErrorCode"], $result["ErrorMsg"]);
         } else {
@@ -477,8 +477,10 @@ class LinkcareSoapAPI {
         $resp = $this->invoke('admission_list_program', $params);
         if (!$resp->getErrorCode()) {
             if ($found = simplexml_load_string($resp->getResult())) {
-                foreach ($found->admission as $taskNode) {
-                    $admissionList[] = APIAdmission::parseXML($taskNode);
+                if (isset($found->admissions)) {
+                    foreach ($found->admissions->admission as $admissionNode) {
+                        $admissionList[] = APIAdmission::parseXML($admissionNode);
+                    }
                 }
             }
         }
